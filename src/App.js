9b493731792c354e04496eb321548969bb274287
lch <lch.insideinfo@gmail.com>
2022-09-19 17:45:14 +0900

    #6 / react useState 실습

--- src/App.js
+++ src/App.js
@@ -10,9 +10,14 @@ const App = () => {
     { title: "New Desk", amount: 500, date: new Date(2022, 9, 28) },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In app.js");
+    console.log(expense);
+  };
+
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
