c51e94f680442a993664db665f82cd51bbea23d4
lch <lch.insideinfo@gmail.com>
2022-09-19 15:05:11 +0900

    #3-2 / 이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
-    { title: "My BirthDay", amount: 500, date: new Date(2022, 9, 28) },
-    { title: "My BirthDay", amount: 500, date: new Date(2022, 9, 28) },
-    { title: "My BirthDay", amount: 500, date: new Date(2022, 9, 28) },
-    { title: "My BirthDay", amount: 500, date: new Date(2022, 9, 28) },
+    { title: "Toilet Paper", amount: 500, date: new Date(2022, 9, 28) },
+    { title: "New TV", amount: 500, date: new Date(2022, 9, 28) },
+    { title: "Car Insurrance", amount: 500, date: new Date(2022, 9, 28) },
+    { title: "New Desk", amount: 500, date: new Date(2022, 9, 28) },
   ];
 
   return (
     <div className="App">
-      <h2>리액트 프로젝트</h2>
-      <p>프로젝트 설명</p>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  //const [현재 상태값, 업데이트하는 함수] = useState(초기값)
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
