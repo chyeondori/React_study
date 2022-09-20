254eec61ecb8ffbd9c9699afb8de91f6dfe6be9c
chyeondori <chyeondori@gmail.com>
2022-09-20 13:40:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  { title: "Toilet Paper", amount: 500, date: new Date(2022, 9, 28) },
+  { title: "New TV", amount: 500, date: new Date(2022, 9, 28) },
+  { title: "Car Insurrance", amount: 500, date: new Date(2022, 9, 28) },
+  { title: "New Desk", amount: 500, date: new Date(2022, 9, 28) },
+];
+
 const App = () => {
-  const expenses = [
-    { title: "Toilet Paper", amount: 500, date: new Date(2022, 9, 28) },
-    { title: "New TV", amount: 500, date: new Date(2022, 9, 28) },
-    { title: "Car Insurrance", amount: 500, date: new Date(2022, 9, 28) },
-    { title: "New Desk", amount: 500, date: new Date(2022, 9, 28) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In app.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,11 @@
+import "./ChartBar.css";
+
+const ChartBar = () => {
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner"></div>
+    </div>
+  );
+};
+
+export default ChartBar;
